149e6b6a8cc2ac0ee19375507ad6f0fcfe179efe
lam0glia <gabslamoglia@outlook.com>
2020-12-08 10:07:47 -0300

    classe Game criada e migration adicionada

--- api_olympic_history/models.py
+++ api_olympic_history/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
-# Create your models here.
+
+class Game(models.Model):
+    season = models.CharField(max_length=6)
+    year = models.IntegerField()
+
+    def __str__(self):
+        return '%s %d' % (self.season, self.year)
