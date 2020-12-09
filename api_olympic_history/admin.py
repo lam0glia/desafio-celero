506bc838f80080b16ad7e1e8951db11591d0e40c
lam0glia <gabslamoglia@outlook.com>
2020-12-08 22:26:11 -0300

    registro de Sport e Event no admin

--- api_olympic_history/admin.py
+++ api_olympic_history/admin.py
@@ -1,5 +1,7 @@
 from django.contrib import admin
-from .models import Game
+from .models import Game, Sport, Event
 
 
 admin.site.register(Game)
+admin.site.register(Sport)
+admin.site.register(Event)
