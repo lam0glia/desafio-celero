e080848514661e1f8dd477178c0c4d784a81842c
lam0glia <gabslamoglia@outlook.com>
2020-12-08 22:25:41 -0300

    Model e Serializer de Sport e Event criados

--- api_olympic_history/models.py
+++ api_olympic_history/models.py
@@ -7,3 +7,18 @@ class Game(models.Model):
 
     def __str__(self):
         return '%s %d' % (self.season, self.year)
+
+
+class Sport(models.Model):
+    name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
+
+
+class Event(models.Model):
+    name = models.CharField(max_length=200)
+    sport = models.ForeignKey(Sport, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.name
--- api_olympic_history/serializers.py
+++ api_olympic_history/serializers.py
@@ -1,8 +1,21 @@
 from rest_framework import serializers
-from .models import Game
+from .models import Game, Sport, Event
 
 
 class GameSerializer(serializers.ModelSerializer):
     class Meta:
         model = Game
+        fields = '__all__'
+
+
+class SportSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Sport
+        fields = '__all__'
+
+
+class EventSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = Event
         fields = '__all__'
