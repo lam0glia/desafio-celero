db37f6251731f632d6a1fa96ab82921fdd6ca577
lam0glia <gabslamoglia@outlook.com>
2020-12-08 22:32:33 -0300

    registro de Medal no admin

--- api_olympic_history/admin.py
+++ api_olympic_history/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
-from .models import Game, Sport, Event
+from .models import Game, Sport, Event, Medal
 
 
 admin.site.register(Game)
 admin.site.register(Sport)
 admin.site.register(Event)
+admin.site.register(Medal)
