5de9591ed719c623b738442bb03d2bb3c850be47
lam0glia <gabslamoglia@outlook.com>
2020-12-10 06:14:17 -0300

    models registradas no admin

--- api_olympic_history/admin.py
+++ api_olympic_history/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
-from .models import Game, Sport, Event, Medal, Athlete, AthleteEvent
-
+from .models import Game, Sport, Event
+from .models import Medal, Athlete, AthleteEvent
+from .models import Team, Noc, Region
 
 admin.site.register(Game)
 admin.site.register(Sport)
@@ -8,3 +9,6 @@ admin.site.register(Event)
 admin.site.register(Medal)
 admin.site.register(Athlete)
 admin.site.register(AthleteEvent)
+admin.site.register(Team)
+admin.site.register(Noc)
+admin.site.register(Region)
