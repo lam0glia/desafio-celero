c7daa6925a81c20c712a9af8acd740d1f4f9e548
lam0glia <gabslamoglia@outlook.com>
2020-12-10 06:28:38 -0300

    paginacao de ViewSets configurada

--- config/settings.py
+++ config/settings.py
@@ -72,6 +72,12 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'config.wsgi.application'
 
+# Ativa paginacao para as ModelViewSets e define como tamanho maximo 100 registros
+
+REST_FRAMEWORK = {
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
+    'PAGE_SIZE': 100
+}
 
 # Database
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
